8c835250f68ccc4e2a4131302786ea79ce725e6c
Raihan Rasheed Apurbo <apurbo97@gmail.com>
2020-09-25 08:22:48 -0400

    experimenting with file input output

---
+++ timetable.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout<<"kill meh"<<endl;
+    vector<vector<int>> courses;
+    freopen("test.txt","r",stdin);
+    int t;
+    // cin>>t;
+    // cout<<t<<endl;
+    while(!cin.eof())
+    {
+        cin>>t;
+        cout<<t<<endl;
+    }
+    // cin>>t;
+    // cout<<t<<endl;
+    // cin>>t;
+    // cout<<t<<endl;
+    // cin>>t;
+    // cout<<t<<endl;
+    // cin>>t;
+    // cout<<t<<endl;
+
+
+
+    // freopen("yor-f-83.stu","r",stdin);
+
+    // freopen("yor-f-83.stu","r",stdin);
+    // int t;
+    // cin>>t;
+    // cout<<t<<endl;
+    // return 0;
+}
